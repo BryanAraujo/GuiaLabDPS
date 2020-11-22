b842ff1cdd081458cc614191d0d63bed381a2bc5
BryanAraujo <bryan.araujo7@hotmail.com>
2020-11-22 16:44:46 -0600

    Subiendo Guia-09

---
+++ Guia09/src/.Firebase.jsx
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBiydbFAYXTxYftCbl_zMLmen7BuVNkSgs",
+    authDomain: "crudreadfirebase.firebaseapp.com",
+    databaseURL: "https://crudreadfirebase.firebaseio.com",
+    projectId: "crudreadfirebase",
+    storageBucket: "crudreadfirebase.appspot.com",
+    messagingSenderId: "658833785827",
+    appId: "1:658833785827:web:d0bb5de2313edaf27416d6"
+  };
