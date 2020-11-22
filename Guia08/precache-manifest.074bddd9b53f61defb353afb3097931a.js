b82515509648ddc633b3a2e418226e7cd167f986
BryanAraujo <bryan.araujo7@hotmail.com>
2020-11-22 15:39:12 -0600

    Subiendo Guia08

---
+++ Guia08/precache-manifest.074bddd9b53f61defb353afb3097931a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f004db320c6e7fc9c220a9b31c3aaa9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a5d17a5c7adf09fcfe2",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a23068ba83df1a5f27dc",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8a5d17a5c7adf09fcfe2",
+    "url": "/static/js/2.e4a8f412.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.e4a8f412.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a23068ba83df1a5f27dc",
+    "url": "/static/js/main.f724455d.chunk.js"
+  },
+  {
+    "revision": "4027bb46236991a061cd",
+    "url": "/static/js/runtime-main.83e6c96f.js"
+  }
+]);
